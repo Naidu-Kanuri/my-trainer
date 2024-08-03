fa52e987e4c58052099e80111f33b8d3883b0015
Sruthipotta <pottasruthi@gmail.com>
2024-08-03 12:37:23 +0530

    Worked on signup implementation

---
+++ src/Components/Signup/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Please Signup</h1>
+      <input type="text" placeholder='Email' /> <br />
+      <input type="password" placeholder='Password' /> <br />
+      <button>Signup</button>
+    </div>
+  )
+}
+
+export default Signup
