d813f9f5c7a6668907428f02bec7ba1fbb73da09
Sruthipotta <pottasruthi@gmail.com>
2024-08-03 12:44:55 +0530

    added styles

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -4,7 +4,7 @@ const Signup = () => {
   return (
     <div>
       <h1>Please Signup</h1>
-      <input type="text" placeholder='Email' /> <br />
+      <input style={{ border: '1px solid black' }} type="text" placeholder='Email' /> <br />
       <input type="password" placeholder='Password' /> <br />
       <button>Signup</button>
     </div>
